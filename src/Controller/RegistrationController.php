e5b96d08a8e397f82ef4dbec62a42c673a8e6e1b
Aymen Chahed <aymen.chahed.13@gmail.com>
2024-05-01 12:41:49 +0100

    Updated some UI

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -60,7 +60,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('tasksphere@hotmail.com', 'ERRET Task Manager'))
+                    ->from(new Address('tasksphere@hotmail.com', 'ERRET Task Sphere'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
